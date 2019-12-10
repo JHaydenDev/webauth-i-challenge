1bcbcbb7e7499d721215770decd624d539d8b7f9
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-12-10 08:52:00 -0600

    Day 1 MVP

--- auth/RequireMiddleware.js
+++ auth/RequireMiddleware.js
@@ -1,34 +1,21 @@
-const router = require('express').Router();
+const Users = require("../users/users-model");
+const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
-
-router.post('/register', (req, res) => {
-  let user = req.body;
-
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-router.post('/login', (req, res) => {
-  let { username, password } = req.body;
-
+const authRequired = (req, res, next) => {
+  const { username, password } = req.headers;
   Users.findBy({ username })
     .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+    .then(_user => {
+      if (_user && bcrypt.compareSync(password, _user.password)) {
+        next();
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        //invalid creds
+        res.status(400).json({ messege: "Invalid Credentials" });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(err => {
+      res.status(500).json({ messege: err });
     });
-});
+};
 
-module.exports = router;
+module.exports = authRequired;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -14,21 +17,18 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "You shall not pass" });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    .catch(err => res.status(500).json(err));
 });
-
 module.exports = router;
