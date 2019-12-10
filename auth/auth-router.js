8b3dcec4181a11a28c3ac11c89d4519f414d85f9
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-12-10 09:27:44 -0600

    added delete to auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const Users = require("../users/users-model");
 
+//post register
 router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 8);
@@ -17,6 +18,7 @@ router.post("/register", (req, res) => {
     });
 });
 
+//post login
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
@@ -24,11 +26,31 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}` });
+        // res.status(200).json({ message: `Welcome ${user.username}` });
+        req.session.user = user;
+        res
+          .status(200)
+          .json({ message: `Welcome ${user.username}, its cookie time!` });
       } else {
         res.status(401).json({ message: "You shall not pass" });
       }
     })
     .catch(err => res.status(500).json(err));
 });
+
+//delete session for logout
+router.delete("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("unable to logout");
+      } else {
+        res.send("Goodbye");
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
 module.exports = router;
